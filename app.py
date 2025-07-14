1d409770a96e7163d6f1b35b666041343c06ec8e
OleksandrDanshyn <s30942@pjwstk.edu.pl>
2025-07-14 19:57:39 +0200

    Get data from api, save to csv, show on page

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import csv
 import os
+from datetime import datetime
+
 from flask import Flask, jsonify
 import requests
 from dotenv import load_dotenv
@@ -7,7 +10,6 @@ app = Flask(__name__)
 def configure():
     load_dotenv(".env")
 
-@app.route('/')
 def get_data():
     api_key = os.getenv('API_KEY')
     url = 'http://api.openweathermap.org/data/2.5/weather'
@@ -22,11 +24,38 @@ def get_data():
     data.raise_for_status()
 
     result = data.json()
-    return jsonify({
+    return {
         "City": result.get('name'),
-        "Temperature": result.get('main', {}).get('temp')
-    })
+        "Temperature": result.get('main', {}).get('temp'),
+        "Feels_like": result.get('main', {}).get('feels_like'),
+        "Humidity": result.get('main', {}).get('humidity'),
+        "Wind": result.get('wind', {}).get('speed'),
+        "Cloudiness": result.get('clouds', {}).get('all'),
+        "Time": datetime.fromtimestamp(result.get('dt'))
+    }
 
+def save_to_csv(data):
+    filename = 'weather.csv'
+    file_exists = os.path.isfile(filename)
+    with open(filename, mode='a', newline='', encoding='utf') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data.keys())
+        if not file_exists:
+            writer.writeheader()
+        writer.writerow(data)
+
+@app.route('/weather')
+def show_weather():
+    data = get_data()
+    print("Weather data requested via web:")
+    for key, value in data.items():
+        print(f"{key}: {value}")
+    return jsonify(data)
 
 if __name__ == '__main__':
+    configure()
+    data = get_data()
+    print("Weather data: ")
+    for key, value in data.items():
+        print(f"{key}: {value}")
+    save_to_csv(data)
     app.run()
